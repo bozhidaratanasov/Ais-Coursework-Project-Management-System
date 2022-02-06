eae6639593a0fb2e6e517717ce31f6bb489668b8
Bozhidar Atanasov <84458027+bozhidaratanasov@users.noreply.github.com>
2022-02-06 19:57:34 +0200

    Added HTML Report

--- src/test/java/task/management/TaskManagementStarter.java
+++ src/test/java/task/management/TaskManagementStarter.java
@@ -2,9 +2,11 @@ package task.management;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(monochrome = true, plugin = {"pretty", "html:target/login-feature"})
 public class TaskManagementStarter {
 
 }
